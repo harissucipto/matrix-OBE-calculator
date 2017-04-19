c521c436c54f9d4bad7b5258b12bf66517ce92d1
anan474 <anan474@myself.com>
2017-04-19 09:50:28 +0700

    lumayan

--- main.js
+++ main.js
@@ -1,66 +1,75 @@
-var cariA = [];
-var CariB = [];
-var CariC = [];
+var cariA = [ ];
+var CariB = [ ];
+var CariC = [ ];
 var konsanta;
 
+var matrixC = document.querySelectorAll( ".C" );
+var fire = document.querySelector( "#fire" );
 
-var matrixC = document.querySelectorAll(".C");
-var fire = document.querySelector("#fire");
-
-
-fire.addEventListener("click", function() {
-  getNilai();
-  CariC = cariJawaban(cariA, cariB, konsanta);
-  tulisHasil(matrixC, CariC);
+fire.addEventListener( "click", function ( ) {
+	getNilai( );
+	CariC = cariJawaban( cariA, cariB, konsanta );
+	tulisHasil( matrixC, CariC );
 });
 
-
-function getNilai() {
-  var matrixA = document.querySelectorAll(".A");
-  var matrixB = document.querySelectorAll(".B");
-  konsanta = document.querySelector("#operasi").value;
-
-  cariA = mapNode(matrixA, convertToFraction);
-  cariB = mapNode(matrixB, convertToFraction);;
+function getNilai( ) {
+	var matrixA = isiArray( ".A" );
+	var matrixB = isiArray( ".B" );
+	konsanta = document
+		.querySelector( "#operasi" )
+		.value || 1;
+	cariA = mapNode( matrixA, convertToFraction );
+	cariB = mapNode( matrixB, convertToFraction );;
 
 }
 
+//ambil nama class trus ambil isi .value tiap itemnya, keluarkan dalam bentuk arr
+//kalo kosong biarkan jadi 0
+//entah kenapa pake arr.map tak bisa
+function isiArray( kelas ) {
+	let arr = document.querySelectorAll( kelas );
+	let newArr = [ ];
 
-function cariJawaban(fracA, fracB, konsanta) {
-  var tempArr = fracA.map((object) => object.mul(konsanta));
-  var results = [];
-  for (var i = 0; i < tempArr.length; i++) {
-    results.push(tempArr[i].add(fracB[i]));
-  }
-  return results;
+	for ( var x = 0; x < arr.length; x++ ) {
+		if ( arr[x]['value' ] == "") {
+			newArr[x] = 0
+		} else {
+			newArr[x] = arr[x]['value' ];
+		}
+	}
+	return newArr;
 }
 
-
-
-function convertToFraction(value) {
-  return new Fraction(value);
+function cariJawaban( fracA, fracB, konsanta ) {
+	var tempArr = fracA.map(( object ) => object.mul( konsanta ));
+	var results = [ ];
+	for ( var i = 0; i < tempArr.length; i++ ) {
+		results.push(tempArr[i].add(fracB[i]));
+	}
+	return results;
 }
 
-
-function mapNode(array, fn) {
-  var results = [];
-  for (var i = 0; i < array.length; i++){
-    results.push(fn(array[i].value));
-  }
-  return results;
+function convertToFraction( value ) {
+	return new Fraction( value );
 }
 
-function tulisHasil(node, hasil) {
-  for (var i  = 0; i < hasil.length; i++) {
-    node[i].value = hasil[i].toFraction(true);
-  }
+function mapNode( array, fn ) {
+	var results = [ ];
+	for ( var i = 0; i < array.length; i++ ) {
+		results.push(fn(array[i]));
+	}
+	return results;
 }
 
-
+function tulisHasil( node, hasil ) {
+	for ( var i = 0; i < hasil.length; i++ ) {
+		node[i].value = hasil[i].toFraction( true );
+	}
+}
 
 /////////////////////// generate banyak kolom /////////////////
 // var banyakKolom = parseInt(document.querySelector("#kolom").value) + 1;
-var generate = document.querySelector("#generate");
+var generate = document.querySelector( "#generate" );
 //
 // var baseA = document.querySelector("#baseA");
 // var baseB = document.querySelector("#baseB");
@@ -77,17 +86,17 @@ var generate = document.querySelector("#generate");
 //
 //
 //
-generate.addEventListener("click", function() {
-  alert("fitur ini belum tersedia");
-//
-//   if (baseA.length < banyakKolom) {
-//     for (var i = matrixC.length; i <= banyakKolom; i++) {
-//       baseA.appendChild(inputA);
-//       baseB.appendChild(inputB);
-//       baseC.appendChild(inputC);
-//     }
-//   } else {
-//
-//   }
-//
+generate.addEventListener( "click", function ( ) {
+	alert( "fitur ini belum tersedia" );
+	//
+	//   if (baseA.length < banyakKolom) {
+	//     for (var i = matrixC.length; i <= banyakKolom; i++) {
+	//       baseA.appendChild(inputA);
+	//       baseB.appendChild(inputB);
+	//       baseC.appendChild(inputC);
+	//     }
+	//   } else {
+	//
+	//   }
+	//
 });
